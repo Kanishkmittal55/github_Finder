83945a0e083a4e0e0c28e41f2abcb347af5fdb6d
Kanishk <kanishkmittal55@gmail.com>
2022-10-24 03:10:50 +0100

    Final Commit

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,84 +1,100 @@
 import { createContext, useReducer } from "react";
+import { createRenderer } from "react-dom/test-utils";
 import githubReducer from "./GithubReducer";
 
+const GithubContext = createContext();
 
-const GithubContext = createContext()
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
 
-export const GithubProvider = ({children}) => {
-    const initialState = {
-        users : [],
-        user: {},
-        loading : false
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+
+  // Get single User
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      // headers: {
+      //   Authorization: `token ${GITHUB_TOKEN}`,
+      // },
+    });
+    if (response.status === 404) {
+      window.location = "/notFound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
     }
+  };
 
-    const [state, dispatch] = useReducer(githubReducer,initialState)
+  // Get User Repos
 
-    // Get search results 
-    const searchUsers = async (text) => {
-        setLoading()
+  const getUserRepos = async (login) => {
+    setLoading();
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
 
-        const params = new URLSearchParams({
-            q: text
-        })
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers:{
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`
+    );
 
-        const {items} = await response.json()
+    const data = await response.json();
 
-        dispatch({
-            type: 'GET_USERS',
-            payload : items
-        })
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
-    }
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        loading: state.loading,
+        repos: state.repos,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
-
-    // Get single User
-    const getUser = async (login) => {
-        setLoading()
-
-        
-        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-            headers:{
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-        if(response.status === 404){
-            window.location ='/notFound'
-        }
-        else{
-            
-            const data = await response.json()
-    
-            dispatch({
-                type: 'GET_USER',
-                payload : data
-            })
-
-        }
-
-
-    }
-    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
-
-
-    const setLoading = () => dispatch({type: 'SET_LOADING'})
-
-    
-    
-
-    return (
-        <GithubContext.Provider value={{users: state.users,user: state.user, loading: state.loading, searchUsers, clearUsers, getUser}}>
-            {children}
-        </GithubContext.Provider>
-    )
-}
-
-export default GithubContext
+export default GithubContext;
